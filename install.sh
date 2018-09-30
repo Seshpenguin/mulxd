fd1e372841a51cc686fdc381b197207310b20da5
Seshpenguin <seshan10@me.com>
2018-09-30 18:25:39 -0400

    Add to .bashrc from the installer

--- install.sh
+++ install.sh
@@ -8,3 +8,16 @@ mkdir -p $install_dir
 cp ./mulxd.sh $install_dir/mulxd.sh
 cp ./motd.txt $install_dir/motd.txt
 cp ./exempt.txt $install_dir/exempt.txt
+
+echo "Done copying files."
+read -p "Do you want to add MULXD to the default bashrc for new users (y/N)? " -n 1 -r
+echo    
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "# MULXD (https://github.com/Seshpenguin/mulxd)" >> /etc/skel/.bashrc
+    echo "$install_dir/mulxd.sh" >> /etc/skel/.bashrc
+    echo "exit" >> /etc/skel/.bashrc
+    echo "Added autostart to /etc/skel/.bashrc"
+fi
+
+echo "Done!"
