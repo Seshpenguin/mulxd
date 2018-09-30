0dc643974308770d70920e46e7f5ee6dadef0776
Seshpenguin <seshan10@me.com>
2018-09-30 13:21:27 -0400

    cp not mv

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ echo "Installing Multi User LXD..."
 install_dir=${1:-"/opt/mulxd"}
 
 mkdir -p $install_dir
-mv ./mulxd.sh $install_dir/mulxd.sh
-mv ./motd.txt $install_dir/motd.txt
-mv ./exempt.txt $install_dir/exempt.txt
+cp ./mulxd.sh $install_dir/mulxd.sh
+cp ./motd.txt $install_dir/motd.txt
+cp ./exempt.txt $install_dir/exempt.txt
